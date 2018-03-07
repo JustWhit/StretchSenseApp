84c2782e76697a03e378cb4e571b432401469700
Justin <jscha002@odu.edu>
2018-03-07 09:21:53 -0500

    Initial Commit

---
+++ StretchSenseApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StretchSenseApp
+//
+//  Created by Justin Whitlock on 3/7/18.
+//  Copyright © 2018 Justin Whitlock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StretchSenseApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StretchSenseApp
+//
+//  Created by Justin Whitlock on 3/7/18.
+//  Copyright © 2018 Justin Whitlock. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
